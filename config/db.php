8155fa51d8eadc68bd28d12c9e224427c4b16b9e
Ogunyemi Richard Ayooluwa <143848799+Lordricho@users.noreply.github.com>
2026-06-16 13:47:38 +0100

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "sqlXXX.infinityfree.com";
-$dbname = "if0_XXXXXX_broker";
-$username = "if0_XXXXXX";
-$password = "YOUR_PASSWORD";
+$host = "sqlXXX.infinityfree.com";   // we will replace this if different
+$dbname = "if0_42196148_broker_db";  // your real database name
+$username = "if0_42196148";          // your DB username
+$password = "YOUR_DB_PASSWORD";
 
 $conn = new mysqli($host, $username, $password, $dbname);
 
